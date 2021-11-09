6177da65bbef2e4b7e10f5ed0e4dcc2392971d24
Rogerio Martins <86385568+rogermds@users.noreply.github.com>
2021-11-09 17:47:03 -0300

    upload the project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const port = process.env.PORT || 3000;
+
+// Connection Details
+const connection = mysql.createConnection({
+	host: "us-cdbr-east-04.cleardb.com",
+	user: "b392262f4cfdfd",
+	password: "7c4ae6ca",
+	database: "heroku_f7227624a830ae6",
+});
+
+// View engine
+app.set("view engine", "ejs");
+
+// Render Home Page
+app.get("/", function (req, res) {
+	connection.query('SELECT * FROM user WHERE id = "1"', (error, rows) => {
+		if (error) throw error;
+
+		if (!error) {
+			console.log(rows);
+			res.render("index", { rows });
+		}
+	});
+});
+
+app.listen(port);
+console.log(`Server is listening on port ${port}`);
